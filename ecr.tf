3452a120fb157175e6db4d483836f27cd08b97f4
yusuf.celebi <yusuf.celebi@ciceksepeti.com>
2021-04-29 11:41:54 +0300

    ECR repositories added to terraform

--- ecr.tf
+++ ecr.tf
@@ -1,4 +1,14 @@
-resource "aws_ecr_repository" "assignment_repo" {
-  name                 = "assignment_repo"
+resource "aws_ecr_repository" "result_repo" {
+  name                 = "result"
+  image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_ecr_repository" "vote_repo" {
+  name                 = "vote"
+  image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_ecr_repository" "worker_repo" {
+  name                 = "worker"
   image_tag_mutability = "MUTABLE"
 }
